6e64f3c79f4df684aede016db25107b6014e2716
Edgar Quintana <edgarquintana984@hotmail.com>
2016-07-18 09:54:42 -0500

    Growing functions -- A function must have an explicit and understandable name that tell us what it does

---
+++ farm_function.js
@@ -0,0 +1,17 @@
+// Make a function that prints in three digits the number of different animals the farmer have --> Inventory
+
+function zeroPad(number, width) {
+  var string = String(number);
+  while (string.length < width) {
+    string = "0" + string;
+  }
+  return string;
+}
+
+function printFarmInventory(cows, chickens, pigs) {
+  console.log(zeroPad(cows, 3) + " Cows");
+  console.log(zeroPad(chickens, 3) + " Chickens");
+  console.log(zeroPad(pigs, 3) + " Pigs");
+}
+
+printFarmInventory(101, 50, 7);
